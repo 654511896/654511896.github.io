b3443428e1cd58cbab19ddba6dc8081a7be78e0c
654511896 <150405125+654511896@users.noreply.github.com>
2023-11-26 05:58:54 +0800

    Add files via upload

---
+++ data.source.js
@@ -0,0 +1,772 @@
+import React from 'react';
+export const Nav30DataSource = {
+  wrapper: { className: 'header3 home-page-wrapper' },
+  page: { className: 'home-page' },
+  logo: {
+    className: 'header3-logo lpei8nrq759-editor_css',
+    children:
+      'https://picdl.sunbangyan.cn/2023/11/26/b6c78ad08d4b734c5eea3c85f0a15e05.jpeg',
+  },
+  Menu: {
+    className: 'header3-menu',
+    children: [
+      {
+        name: 'item0',
+        className: 'header3-item',
+        children: {
+          href: '#',
+          children: [
+            {
+              children: (
+                <span>
+                  <span>
+                    总负责人<br />
+                  </span>
+                </span>
+              ),
+              name: 'text',
+            },
+          ],
+        },
+        subItem: [
+          {
+            name: 'sub0',
+            className: 'item-sub',
+            children: {
+              className: 'item-sub-item',
+              children: [
+                {
+                  name: 'title',
+                  className: 'item-title',
+                  children: (
+                    <span>
+                      天子大帅<br />
+                    </span>
+                  ),
+                },
+                {
+                  name: 'content',
+                  className: 'item-content',
+                  children: (
+                    <span>
+                      网站设计者<br />
+                    </span>
+                  ),
+                },
+              ],
+            },
+          },
+        ],
+      },
+      {
+        name: 'item1',
+        className: 'header3-item',
+        children: {
+          href: '#',
+          children: [
+            {
+              children: (
+                <span>
+                  天子大帅<br />
+                </span>
+              ),
+              name: 'text',
+            },
+          ],
+        },
+      },
+      {
+        name: 'item2',
+        className: 'header3-item',
+        children: {
+          href: '#',
+          children: [
+            {
+              children: (
+                <span>
+                  成果展示<br />
+                </span>
+              ),
+              name: 'text',
+            },
+          ],
+        },
+      },
+      {
+        name: 'item3',
+        className: 'header3-item',
+        children: {
+          href: '#',
+          children: [
+            {
+              children: (
+                <span>
+                  萧萧BOT<br />
+                </span>
+              ),
+              name: 'text',
+            },
+          ],
+        },
+      },
+    ],
+  },
+  mobileMenu: { className: 'header3-mobile-menu' },
+};
+export const Banner01DataSource = {
+  wrapper: { className: 'banner0 lpe7k7efrd-editor_css' },
+  textWrapper: { className: 'banner0-text-wrapper lpe7r35r5oq-editor_css' },
+  title: {
+    className: 'banner0-title lpe7lx1v4jl-editor_css',
+    children: 'https://z1.ax1x.com/2023/11/25/pi01iNT.png',
+  },
+  content: {
+    className: 'banner0-content lpe8y3maumo-editor_css',
+    children: (
+      <span>
+        <span>
+          一个AI科技与中医学科碰撞的产物<br />
+        </span>
+      </span>
+    ),
+  },
+  button: {
+    className: 'banner0-button lpe8yua7chb-editor_css',
+    children: (
+      <span>
+        <span>
+          <div>萧苍若_Bot</div>
+          <div>
+            <p>
+              <br />
+            </p>
+          </div>
+        </span>
+      </span>
+    ),
+    href: '',
+    type: 'primary',
+  },
+};
+export const Content00DataSource = {
+  wrapper: { className: 'home-page-wrapper content0-wrapper' },
+  page: { className: 'home-page content0 lpecw0h2s4i-editor_css' },
+  OverPack: { playScale: 0.3, className: '' },
+  titleWrapper: {
+    className: 'title-wrapper',
+    children: [
+      {
+        name: 'title',
+        children: (
+          <span>
+            产品优势<br />
+          </span>
+        ),
+      },
+    ],
+  },
+  childWrapper: {
+    className: 'content0-block-wrapper lped973zpxe-editor_css',
+    children: [
+      {
+        name: 'block0',
+        className: 'content0-block',
+        md: 8,
+        xs: 24,
+        children: {
+          className: 'content0-block-item',
+          children: [
+            {
+              name: 'image',
+              className: 'content0-block-icon',
+              children:
+                'https://zos.alipayobjects.com/rmsportal/WBnVOjtIlGWbzyQivuyq.png',
+            },
+            {
+              name: 'title',
+              className: 'content0-block-title',
+              children: (
+                <span>
+                  <p>
+                    高速响应服务<br />
+                  </p>
+                </span>
+              ),
+            },
+            {
+              name: 'content',
+              children: (
+                <span>
+                  <span>
+                    <div>
+                      <span>基于阿里云框架的超强算力</span>
+                    </div>
+                    <div>
+                      <p>
+                        <span>&nbsp;告别长时间等待</span>
+                      </p>
+                    </div>
+                    <div>
+                      <span />
+                    </div>
+                  </span>
+                </span>
+              ),
+            },
+          ],
+        },
+      },
+      {
+        name: 'block1',
+        className: 'content0-block',
+        md: 8,
+        xs: 24,
+        children: {
+          className: 'content0-block-item',
+          children: [
+            {
+              name: 'image',
+              className: 'content0-block-icon',
+              children:
+                'https://zos.alipayobjects.com/rmsportal/YPMsLQuCEXtuEkmXTTdk.png',
+            },
+            {
+              name: 'title',
+              className: 'content0-block-title',
+              children: (
+                <span>
+                  <span>
+                    大型NLP自然语言模型<br />
+                  </span>
+                </span>
+              ),
+            },
+            {
+              name: 'content',
+              children: (
+                <span>
+                  <span>
+                    <div>
+                      <span>拥有强大的归纳和检索能力</span>
+                    </div>
+                    <div>
+                      <p>
+                        <span>大大降低使用门槛</span>
+                      </p>
+                    </div>
+                    <div>
+                      <span />
+                    </div>
+                  </span>
+                </span>
+              ),
+            },
+          ],
+        },
+      },
+      {
+        name: 'block2',
+        className: 'content0-block',
+        md: 8,
+        xs: 24,
+        children: {
+          className: 'content0-block-item',
+          children: [
+            {
+              name: 'image',
+              className: 'content0-block-icon',
+              children:
+                'https://zos.alipayobjects.com/rmsportal/EkXWVvAaFJKCzhMmQYiX.png',
+            },
+            {
+              name: 'title',
+              className: 'content0-block-title',
+              children: (
+                <span>
+                  <span>
+                    垂直精细化输出<br />
+                  </span>
+                </span>
+              ),
+            },
+            {
+              name: 'content',
+              children: (
+                <span>
+                  <span>
+                    <div>对中医学古籍有着更专业的表达</div>
+                    <div>
+                      <p> </p>
+                      <p>
+                        摒弃低能搜索<br />
+                      </p>
+                      <p>
+                        <br />
+                      </p>
+                    </div>
+                  </span>
+                </span>
+              ),
+            },
+          ],
+        },
+      },
+    ],
+  },
+};
+export const Feature10DataSource = {
+  wrapper: { className: 'home-page-wrapper content1-wrapper' },
+  OverPack: {
+    className: 'home-page content1 lped73sye1m-editor_css',
+    playScale: 0.3,
+  },
+  imgWrapper: { className: 'content1-img', md: 10, xs: 24 },
+  img: {
+    children:
+      'https://picdl.sunbangyan.cn/2023/11/26/1363981f748658f18d537cdb1cb23f9f.jpeg',
+    className: 'lpeh2km9cd7-editor_css',
+  },
+  textWrapper: {
+    className: 'content1-text lpebe7l78zp-editor_css',
+    md: 14,
+    xs: 24,
+  },
+  title: {
+    className: 'content1-title lpea605j7dh-editor_css',
+    children: (
+      <span>
+        <span>
+          <span>
+            <span>
+              与传统搜索引擎相比,我们<br />
+            </span>
+          </span>
+        </span>
+      </span>
+    ),
+  },
+  content: {
+    className: 'content1-content lpearcce2ep-editor_css',
+    children: (
+      <span>
+        <span>
+          <span>
+            <span>
+              <span>
+                <span>
+                  <span>
+                    <div>
+                      <span>没有广告! 没有引流! 不会跳转到淘宝!</span>
+                    </div>
+                  </span>
+                </span>
+              </span>
+            </span>
+          </span>
+          <p>
+            <br />
+          </p>
+          <span>
+            <span>
+              <span>
+                <span>
+                  <div>
+                    <span>基于开发者定向数据训练,输出的结果准确</span>
+                  </div>
+                </span>
+              </span>
+            </span>
+          </span>
+          <p>
+            <br />
+          </p>
+          <span>
+            <span>
+              <span>
+                <p>超强适应口语化表达的能力,与古籍对话,实现跨时空交流</p>
+                <br />
+                <br />
+                <span>
+                  <div>
+                    <p>
+                      <span>
+                        <br />
+                      </span>
+                    </p>
+                  </div>
+                  <span>
+                    <div>
+                      <p>
+                        <br />
+                        <span />
+                      </p>
+                    </div>
+                  </span>
+                </span>
+              </span>
+            </span>
+          </span>
+        </span>
+      </span>
+    ),
+  },
+};
+export const Feature20DataSource = {
+  wrapper: { className: 'home-page-wrapper content2-wrapper' },
+  OverPack: {
+    className: 'home-page content2 lped6jwc5qb-editor_css',
+    playScale: 0.3,
+  },
+  imgWrapper: {
+    className: 'content2-img lpedbq0mj38-editor_css',
+    md: 10,
+    xs: 24,
+  },
+  img: {
+    children:
+      'https://picst.sunbangyan.cn/2023/11/26/36a123db9c8f5d62a95f640ce2351046.jpeg',
+    className: 'lpeh3bmef5-editor_css',
+  },
+  textWrapper: {
+    className: 'content2-text lpeazaal0n-editor_css',
+    md: 14,
+    xs: 24,
+  },
+  title: {
+    className: 'content2-title',
+    children: (
+      <span>
+        <p>与传统ChatGPT相比,我们</p>
+      </span>
+    ),
+  },
+  content: {
+    className: 'content2-content lpebf7o5ivn-editor_css',
+    children: (
+      <span>
+        <span>
+          <span>
+            <span>
+              <span>
+                <div>
+                  <span>
+                    <span>
+                      完善的可视化后端系统,能对输出结果进行快速干预,改变语言风格
+                    </span>
+                  </span>
+                </div>
+              </span>
+              <p>
+                <br />
+              </p>
+              <span>
+                <div>
+                  <p>
+                    <span>
+                      <span>
+                        拥有纯净数据库,超高的向量分类检索能力,实现答你所问,问有所得
+                      </span>
+                    </span>
+                  </p>
+                  <p>
+                    <span>
+                      <span>
+                        <br />
+                      </span>
+                    </span>
+                  </p>
+                  <p>
+                    <span>
+                      <span>
+                        垂直于中医学科,专攻于中医学科,更适合中医宝宝体质的心理咨熊师<br />
+                      </span>
+                    </span>
+                  </p>
+                  <p>
+                    <span>
+                      <span>
+                        <br />
+                      </span>
+                    </span>
+                  </p>
+                  <p>
+                    <span>
+                      <span>
+                        <br />
+                      </span>
+                    </span>
+                  </p>
+                  <p>
+                    <span>
+                      <span>
+                        <br />
+                      </span>
+                    </span>
+                  </p>
+                </div>
+              </span>
+              <span>
+                <div>
+                  <span>
+                    <span />
+                  </span>
+                </div>
+              </span>
+            </span>
+          </span>
+        </span>
+      </span>
+    ),
+  },
+};
+export const Content50DataSource = {
+  wrapper: {
+    className: 'home-page-wrapper content5-wrapper lped24kavb-editor_css',
+  },
+  page: { className: 'home-page content5 lpeczd6on5f-editor_css' },
+  OverPack: { playScale: 0.3, className: '' },
+  titleWrapper: {
+    className: 'title-wrapper',
+    children: [
+      {
+        name: 'title',
+        children: (
+          <span>
+            六海成员<br />
+          </span>
+        ),
+        className: 'title-h1',
+      },
+      {
+        name: 'content',
+        className: 'title-content lpe9txvjmd9-editor_css',
+        children: (
+          <span>
+            <span>
+              <span>
+                <p>
+                  <span>
+                    <span>
+                      <span>
+                        萧苍若,一个心理咨熊师,曾帮助1122个人走出正常家庭
+                      </span>
+                    </span>
+                  </span>
+                </p>
+              </span>
+            </span>
+          </span>
+        ),
+      },
+    ],
+  },
+  block: {
+    className: 'content5-img-wrapper lpedba9gp8q-editor_css',
+    gutter: 16,
+    children: [
+      {
+        name: 'block0',
+        className: 'block',
+        md: 6,
+        xs: 24,
+        children: {
+          wrapper: { className: 'content5-block-content' },
+          img: {
+            children:
+              'https://picdl.sunbangyan.cn/2023/11/26/1363981f748658f18d537cdb1cb23f9f.jpeg',
+            className: 'lpe95vvto3-editor_css',
+          },
+          content: {
+            children: (
+              <span>
+                邱天韵<br />
+              </span>
+            ),
+          },
+        },
+      },
+      {
+        name: 'block1',
+        className: 'block',
+        md: 6,
+        xs: 24,
+        children: {
+          wrapper: { className: 'content5-block-content' },
+          img: {
+            children:
+              'https://picst.sunbangyan.cn/2023/11/26/36a123db9c8f5d62a95f640ce2351046.jpeg',
+            className: 'lpe97lgdh7u-editor_css',
+          },
+          content: {
+            children: (
+              <span>
+                陆亚玲<br />
+              </span>
+            ),
+          },
+        },
+      },
+      {
+        name: 'block~lpe9tgrvamj',
+        className: 'block',
+        md: 6,
+        xs: 24,
+        children: {
+          wrapper: { className: 'content5-block-content' },
+          img: {
+            children:
+              'https://picdm.sunbangyan.cn/2023/11/26/2efe5ed61f68049fceb7f70778ccc992.jpeg',
+            className: 'lpe9k66o2rd-editor_css',
+          },
+          content: {
+            children: (
+              <span>
+                奚堉钰<br />
+              </span>
+            ),
+          },
+        },
+      },
+      {
+        name: 'block3',
+        className: 'block',
+        md: 6,
+        xs: 24,
+        children: {
+          wrapper: { className: 'content5-block-content' },
+          img: {
+            children:
+              'https://picdm.sunbangyan.cn/2023/11/26/bf0f53a2db1b30121d09c2c044cdc81b.jpeg',
+            className: 'lpe9qe3h1a7-editor_css',
+          },
+          content: {
+            children: (
+              <span>
+                孙雅涵<br />
+              </span>
+            ),
+          },
+        },
+      },
+      {
+        name: 'block4',
+        className: 'block',
+        md: 6,
+        xs: 24,
+        children: {
+          wrapper: { className: 'content5-block-content' },
+          img: {
+            children:
+              'https://picdl.sunbangyan.cn/2023/11/26/4202b740e5f19ff7e6f3f43dab164de4.jpeg',
+            className: 'lpe9qolvyeq-editor_css',
+          },
+          content: {
+            children: (
+              <span>
+                杨伊媛<br />
+              </span>
+            ),
+          },
+        },
+      },
+      {
+        name: 'block5',
+        className: 'block',
+        md: 6,
+        xs: 24,
+        children: {
+          wrapper: { className: 'content5-block-content' },
+          img: {
+            children:
+              'https://picdl.sunbangyan.cn/2023/11/26/6d353395a55123cc3edfe079333e86c2.jpeg',
+            className: 'lpe9kjk1qrs-editor_css',
+          },
+          content: {
+            children: (
+              <span>
+                邓湘芸<br />
+              </span>
+            ),
+          },
+        },
+      },
+      {
+        name: 'block6',
+        className: 'block',
+        md: 6,
+        xs: 24,
+        children: {
+          wrapper: { className: 'content5-block-content' },
+          img: {
+            children:
+              'https://picst.sunbangyan.cn/2023/11/26/dc8a17bcd28e314ffe6442491936bcdb.jpeg',
+            className: 'lpe9kz5mer9-editor_css',
+          },
+          content: {
+            children: (
+              <span>
+                温文君<br />
+              </span>
+            ),
+          },
+        },
+      },
+      {
+        name: 'block~lpedgvlfb6h',
+        className: 'block',
+        md: 6,
+        xs: 24,
+        children: {
+          wrapper: {
+            className: 'content5-block-content lpedllw6gcj-editor_css',
+          },
+          img: {
+            children:
+              'https://picst.sunbangyan.cn/2023/11/26/93eeec85393c73c7215ba1ac4e3bde84.jpeg',
+            className: 'lpe9kz5mer9-editor_css',
+          },
+          content: {
+            children: (
+              <span>
+                陈佳怡<br />
+              </span>
+            ),
+          },
+        },
+      },
+    ],
+  },
+};
+export const Footer00DataSource = {
+  wrapper: { className: 'home-page-wrapper footer0-wrapper' },
+  OverPack: { className: 'home-page footer0', playScale: 0.05 },
+  copyright: {
+    className: 'copyright',
+    children: (
+      <span>
+        <u>
+          <span>
+            <span>
+              <blockquote>
+                <span>
+                  <span>
+                    <span>
+                      ©2023{' '}
+                      <a
+                        href="https://landing.ant.design/edit/index-cn#uid=656205516aa58e39d922df84"
+                        target="_blank"
+                      >
+                        <b>天子大帅</b>
+                      </a>{' '}
+                      All Rights Reserved
+                    </span>
+                  </span>
+                </span>
+              </blockquote>
+            </span>
+          </span>
+        </u>
+      </span>
+    ),
+  },
+};
